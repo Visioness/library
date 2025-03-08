355a523d0c7a784f9d4ef742bf90785806f3eae7
Visioness <aygun-08-zurnaci@hotmail.com>
2025-03-08 06:08:11 +0300

    Change book styling

--- script.js
+++ script.js
@@ -6,13 +6,8 @@ const form = document.querySelector("form");
 const closeButton = document.querySelector("#close");
 
 
-addBookToLibrary("The Hobbit", "J.R.R. Tolkien", "295");
-addBookToLibrary("The Hobbit", "J.R.R. Tolkien", "295");
-addBookToLibrary("The Hobbit", "J.R.R. Tolkien", "295");
-addBookToLibrary("The Hobbit", "J.R.R. Tolkien", "295");
-addBookToLibrary("The Hobbit", "J.R.R. Tolkien", "295");
-addBookToLibrary("The Hobbit", "J.R.R. Tolkien", "295");
-addBookToLibrary("The Hobbit", "J.R.R. Tolkien", "295");
+addBookToLibrary("The Hobbit", "J.R.R. Tolkien", "600");
+addBookToLibrary("Harry Potter", "J.K. Rowling", "700");
 displayLibrary()
 
 newBookButton.addEventListener("click", () => {
@@ -24,13 +19,13 @@ closeButton.addEventListener("click", () => {
 });
 
 libraryElement.addEventListener("click", (event) => {
-  if (["read", "button"].every(cls => event.target.classList.contains(cls))) {
+  if (event.target.classList.contains("button-read")) {
     const parent = event.target.parentNode.parentNode;
     parent.book.toggleRead();
     displayLibrary();
   }
 
-  else if (["remove", "button"].every(cls => event.target.classList.contains(cls))) {
+  else if (event.target.classList.contains("button-remove")) {
     const parent = event.target.parentNode.parentNode;
     const index = library.findIndex(book => book.id === parent.dataset.id);
     if (index !== -1) {
@@ -80,9 +75,13 @@ function displayLibrary() {
 
   library.forEach(book => {
     const bookElement = document.createElement("li");
+    const properties = document.createElement("div");
     const title = document.createElement("span");
+    const labelAuthor = document.createElement("label");
     const author = document.createElement("span");
+    const labelPages = document.createElement("label");
     const pages = document.createElement("span");
+    const labelRead = document.createElement("label");
     const read = document.createElement("span");
     
     const readButton = document.createElement("button");
@@ -93,10 +92,12 @@ function displayLibrary() {
     author.classList.add("author");
     pages.classList.add("pages");
     read.classList.add("read");
-    readButton.classList.add("button", "read")
-    removeButton.classList.add("button", "remove")
+    [labelAuthor, labelPages, labelRead].forEach(label => {label.classList.add("label")});
+    readButton.classList.add("button-read")
+    removeButton.classList.add("button-remove")
     buttonsDiv.classList.add("buttons");
     bookElement.classList.add("book");
+    properties.classList.add("properties");
 
     title.textContent = book.title;
     author.textContent = book.author;
@@ -104,14 +105,22 @@ function displayLibrary() {
     read.textContent = book.read ? "Read!" : "Not read yet!";
     readButton.textContent = "Read";
     removeButton.textContent = "Remove";
+    labelAuthor.textContent = "Who is the author?";
+    labelPages.textContent = "How many pages?";
+    labelRead.textContent = "Have you read?";
     
     buttonsDiv.appendChild(readButton);
     buttonsDiv.appendChild(removeButton);
 
+    properties.appendChild(labelAuthor);
+    properties.appendChild(author);
+    properties.appendChild(labelPages);
+    properties.appendChild(pages);
+    properties.appendChild(labelRead);
+    properties.appendChild(read);
+    
     bookElement.appendChild(title);
-    bookElement.appendChild(author);
-    bookElement.appendChild(pages);
-    bookElement.appendChild(read);
+    bookElement.appendChild(properties);
     bookElement.appendChild(buttonsDiv);
 
     bookElement.book = book;
